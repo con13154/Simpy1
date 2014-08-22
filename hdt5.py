75ee44526e13f840be9e4b2c66592d4dd7ef93f1
con13154 <con13154@uvg.edu.gt>
2014-08-22 07:56:57 -0600

    Hoja de Trabajo 5

---
+++ hdt5.py
@@ -0,0 +1,33 @@
+"""
+Isa Contreras Sandoval 13154
+José Sagastume Pinto   13217
+Hoja de Trabajo 5
+"""
+
+import random
+import simpy
+
+Random_Seed1 = 10
+Random_Seed2 = 2
+Procesos     = 25
+Intervalo    = 10
+Memoria      = 100 
+Instrucciones=3
+Contador=0
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+print('SIMULACIÓN DE SO')
+env = simpy.Environment()
+
